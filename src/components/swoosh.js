0e935ccb03dbc3e8252fa2e934a86a942a49a4d3
icarus <ellishogan95@gmail.com>
2020-06-26 20:49:15 -0400

    new

--- src/components/swoosh.js
+++ src/components/swoosh.js
@@ -1,22 +1,33 @@
 import React, { useEffect } from 'react'
 import {dotsMovement} from '../assets/js/dots.es.js';
 
-
+const debounce = (func, wait) => {
+	let timeout;
+	return function executedFunction(...args) {
+		const later = () => {
+			timeout = null;
+			func(...args);
+		};
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+	};
+};
+  
 const Swoosh = (props) => {
 	useEffect(() => {
         let move = document.getElementById(props.id);
-		dotsMovement(move, [props.startEl, props.endEl], [-6, 360], 1000, "cubicX", ["#009de4", "#06f3a0", "#f30659"]);       
+		debounce(dotsMovement(move, [props.startEl, props.endEl], [-6, 360], 1000, "cubicX", ["#009de4", "#06f3a0", "#f30659"]), 100);       
     });    
     return (
-        <div className="col-12">
-            <div id={ props.id }>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-            </div>						
-        </div>
-    )
+			<div className="col-12">
+				<div id={ props.id }>
+					<div className="dot"></div>
+					<div className="dot"></div>
+					<div className="dot"></div>
+					<div className="dot"></div>
+					<div className="dot"></div>
+				</div>						
+			</div>
+	)
 }
 export default Swoosh;
